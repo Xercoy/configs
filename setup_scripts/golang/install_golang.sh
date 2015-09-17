c38187e16de5ce879b44f8354f89068c5feb0e5c
Xercoy <corey@prak.me>
2015-09-17 09:13:29 -0400

    golang + personal_configs.sh: updated

--- setup_scripts/golang/install_golang.sh
+++ setup_scripts/golang/install_golang.sh
@@ -1,15 +1,23 @@
+# Installs Go to specific destination.
+# Will add args for custom install later. 
 
-# Installs Go 1.5
-wget https://storage.googleapis.com/golang/go1.5.1.src.tar.gz
+# Downloads and extracts Go 1.5.1
+GO_TAR=go1.5.1.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/${GO_TAR}
+tar -xvf ${GO_TAR}
 
-tar -xvf go1.5.1.src.tar.gz
+# Remove previous installation
+rm -r /usr/local/bin/go
 mv go /usr/local/bin/
 
-print "\nDONE, remember to set env vars.\n"
-print "export GOROOT=~/usr/local/bin/go;\n"
-print "export PATH=$PATH:$GOROOT/bin;\n"
-print "export GOPATH=<PATH>"
-print "REMEMBER, GOPATH has src bin pkg dirs."
+echo "\nInstallation complete, remember to set env vars.\n"
+echo "export GOROOT=/usr/local/bin/go;\n"
+echo "export PATH=$PATH:$GOROOT/bin;\n"
+echo "export GOPATH=<PATH>\n"
+echo "REMEMBER, GOPATH has src bin pkg dirs.\n"
+
+# Cleanup
+rm ${GO_TAR}
 
 # Remember to export env vars
 # export GOROOT=$HOME/go
--- setup_scripts/personal_configs.sh
+++ setup_scripts/personal_configs.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 
 # Git install and user setup
- sudo apt-get install -y git
- git config --global user.email "corey@prak.me"
- git config --global user.name "Xercoy"
+sudo apt-get install -y git
+git config --global user.email "corey@prak.me"
+git config --global user.name "Xercoy"
+
