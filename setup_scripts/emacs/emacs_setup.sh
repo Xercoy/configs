63358d1afff0692f6767cd94cdba3daa26ae82fb
Xercoy <corey@prak.me>
2015-09-21 17:18:35 -0400

    setup_scripts/emacs:Modified emacs setup and init script.

--- setup_scripts/emacs/emacs_setup.sh
+++ setup_scripts/emacs/emacs_setup.sh
@@ -1,5 +1,9 @@
 sudo apt-get install -y emacs
 
+# Needed for color theme
+# http://www.nongnu.org/color-theme/index.html
+audo apt-get install -y emacs-goodies-el
+
 mkdir -p ~/.emacs.d/
 cp ./init.el ~/.emacs.d/init.el
 
--- setup_scripts/emacs/init.el
+++ setup_scripts/emacs/init.el
@@ -1,5 +1,23 @@
-
 ; Go mode
 ; https://github.com/dominikh/go-mode.el
 (add-to-list 'load-path "~/.emacs.d/packages/go-mode/")
 (require 'go-mode-autoloads)
+
+;Color theme
+(require 'color-theme)
+    (color-theme-initialize)
+; looks bad on ssh    (color-theme-dark-blue2)
+    (color-theme-jsc-dark)
+;Whitespace
+(require 'whitespace)
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
+
+;ELPA
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+  t)
+  (package-initialize))
