c9a065f63d120610d97320dfdcf5588c16afa9f4
Corey Prak <coreyprak@Coreys-Mac-mini.local>
2016-07-18 20:45:03 -0700

    Changed install script to download golang 1.6.3 and setup workspace. Very RAW.

--- setup_scripts/golang/install_golang.sh
+++ setup_scripts/golang/install_golang.sh
@@ -1,23 +1,37 @@
 # Installs Go to specific destination.
 # Will add args for custom install later. 
 
-# Downloads and extracts Go 1.5.1
-GO_TAR=go1.5.1.linux-amd64.tar.gz
+# Downloads and extracts Go 1.6.3
+# https://storage.googleapis.com/golang/go1.6.3.src.tar.gz
+
+# OSX doesn't have bin installed. 
+sudo mkdir /usr/local/bin
+
+# GO_TAR=go1.5.1.linux-amd64.tar.gz
+GO_TAR=go1.6.3.darwin-amd64.tar.gz
 wget https://storage.googleapis.com/golang/${GO_TAR}
 tar -xvf ${GO_TAR}
+
+# Cleanup
+rm ${GO_TAR}
 
 # Remove previous installation
 rm -r /usr/local/bin/go
 mv go /usr/local/bin/
 
-echo "\nInstallation complete, remember to set env vars.\n"
-echo "export GOROOT=/usr/local/bin/go;\n"
-echo "export PATH=$PATH:$GOROOT/bin;\n"
-echo "export GOPATH=<PATH>\n"
-echo "REMEMBER, GOPATH has src bin pkg dirs.\n"
 
-# Cleanup
-rm ${GO_TAR}
+# Setup Workspace
+cd ~/
+mkdir workspace
+cd workspace
+mkdir bin src pkg
+
+printf "\nInstallation complete, remember to set env vars.\n"
+echo "export GOROOT=/usr/local/bin/go"
+echo "export PATH=$PATH:$GOROOT/bin"
+echo "export GOPATH=~/workspace"
+echo "export GOBIN=$GOPATH/bin"
+# echo "REMEMBER, GOPATH has src bin pkg dirs.\n"
 
 # Remember to export env vars
 # export GOROOT=$HOME/go
