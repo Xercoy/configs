85361e360eb74f9e63444176f4c5dfbeb2b58abe
Xercoy <corey@prak.me>
2016-11-26 01:19:49 -0800

    scripts/pip: added pip install script

---
+++ setup_scripts/pip/install.sh
@@ -0,0 +1,14 @@
+# Installs pip, easy_install, and virtual_env
+# http://www.saltycrane.com/blog/2010/02/how-install-pip-ubuntu/
+
+# Easy install
+sudo apt-get install python-setuptools python-dev build-essential
+
+# pip
+sudo easy_install pip
+
+# sudo pip install --upgrade virtualenv
+sudo pip install --upgrade virtualenv
+
+# update pip
+sudo pip install --upgrade pip
